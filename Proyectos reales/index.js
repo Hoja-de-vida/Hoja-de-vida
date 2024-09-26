2347d09fa53cd7a36c17ae3341e879ba3fc70556
Hoja-de-vida <est.wvalarezo@smart.edu.co>
2024-09-25 23:46:05 -0500

    Add files via upload

---
+++ Proyectos reales/index.js
@@ -0,0 +1,13 @@
+function sumaDeNumeros(a, b) {
+    return a + b;
+}
+
+// Ejemplo de uso
+let resultado = sumaDeNumeros(5, 3);
+console.log("La suma es:", resultado);
+let sum = resultado 
+console.log ("El suma de uso:", sum);
+class suma extends Array import { sum } from "../Hoja-de-vida";
+console.log ("El suma de uso:", sum);
+
+
